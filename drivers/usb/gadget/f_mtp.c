c36bc753c70c3f6b92f3b4b4600967c169531ac5
maxl <maxl@4b8ad20e-30ca-41b1-81b8-3c8b9e269e15>
2012-12-01 00:04:59 +0000

    remove quasseling log

--- drivers/usb/gadget/f_mtp.c
+++ drivers/usb/gadget/f_mtp.c
@@ -305,7 +305,7 @@ static void mtp_setup_perflock(struct work_struct *data)
 	/* reset the timer */
 	del_timer(&dev->perf_timer);
 	if (dev->mtp_perf_lock_on) {
-		printk(KERN_INFO "[USB][MTP] %s, perf on\n", __func__);
+		//printk(KERN_INFO "[USB][MTP] %s, perf on\n", __func__);
 		if (release_screen_off_flag) {
 			release_screen_off_freq_lock(PM_QOS_CPU_FREQ_MAX_DEFAULT_VALUE);
 			release_screen_off_flag = 0;
@@ -314,7 +314,7 @@ static void mtp_setup_perflock(struct work_struct *data)
 		pm_qos_update_request(&req_cpus, (s32)PM_QOS_MAX_ONLINE_CPUS_DEFAULT_VALUE);
 
 	} else {
-		printk(KERN_INFO "[USB][MTP] %s, perf off\n", __func__);
+		//printk(KERN_INFO "[USB][MTP] %s, perf off\n", __func__);
 		pm_qos_update_request(&mtp_req_freq, (s32)PM_QOS_CPU_FREQ_MIN_DEFAULT_VALUE);
 		pm_qos_update_request(&req_cpus, (s32)PM_QOS_MIN_ONLINE_CPUS_DEFAULT_VALUE);
 		if (!release_screen_off_flag) {
