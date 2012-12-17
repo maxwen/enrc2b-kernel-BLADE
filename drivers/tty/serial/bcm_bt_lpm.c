9571f42f639a880896845590b88a86e03babec51
maxl <maxl@4b8ad20e-30ca-41b1-81b8-3c8b9e269e15>
2012-12-17 00:57:13 +0000

    removed debug

--- drivers/tty/serial/bcm_bt_lpm.c
+++ drivers/tty/serial/bcm_bt_lpm.c
@@ -10,7 +10,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
-#define DEBUG
+//#define DEBUG
 
 #include <linux/module.h>
 #include <linux/platform_device.h>
