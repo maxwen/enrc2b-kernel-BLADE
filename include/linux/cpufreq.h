e9ce633ca5e2cb46fc84db41c1e5e3e7c3b7544d
maxl <maxl@4b8ad20e-30ca-41b1-81b8-3c8b9e269e15>
2013-02-19 21:32:04 +0000

    smartmax: allow using as default governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -373,6 +373,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND1)
 extern struct cpufreq_governor cpufreq_gov_ondemand1;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand1)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 #endif
 
 
