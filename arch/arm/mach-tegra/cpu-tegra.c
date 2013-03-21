423cdb05eace4747b4d4df2fcc4751a601df7d6a
maxl <maxl@4b8ad20e-30ca-41b1-81b8-3c8b9e269e15>
2013-03-21 00:53:00 +0000

    tegra: use cpuquiet force gmode on resume

--- arch/arm/mach-tegra/cpu-tegra.c
+++ arch/arm/mach-tegra/cpu-tegra.c
@@ -55,6 +55,10 @@
 extern int mpdecision_gmode_notifier(void);
 #endif
 
+#ifdef CONFIG_TEGRA_CPUQUIET
+extern void tegra_cpuquiet_force_gmode(void);
+#endif
+
 unsigned int tegra_pmqos_boost_freq = BOOST_CPU_FREQ_MIN;
 
 /* frequency cap used during suspend (screen off)*/
@@ -630,8 +634,7 @@ int tegra_update_cpu_speed(unsigned long rate)
 #ifndef CONFIG_TEGRA_CPUQUIET
 	if (rate_save > T3_LP_MAX_FREQ) {
 		if (is_lp_cluster()) {
-
-			pr_info("LP off %d %d %ld\n", freqs.old, freqs.new, rate_save);
+			pr_info("tegra_update_cpu_speed: LP off %d %d %ld\n", freqs.old, freqs.new, rate_save);
 			/* set rate to max of LP mode */
 			ret = clk_set_rate(cpu_clk, T3_LP_MAX_FREQ * 1000);
 #ifndef CONFIG_TEGRA_MPDECISION
@@ -645,7 +648,7 @@ int tegra_update_cpu_speed(unsigned long rate)
              */
              status = mpdecision_gmode_notifier();
              if (status == 0)
-             	pr_err("%s: couldn't switch to gmode (freq)", __func__ );
+             	pr_err("tegra_update_cpu_speed: %s: couldn't switch to gmode (freq)", __func__ );
 #endif
 			/* restore the target frequency, and
 			 * let the rest of the function handle
@@ -667,13 +670,13 @@ int tegra_update_cpu_speed(unsigned long rate)
 	if (freqs.old < freqs.new) {
 		ret = tegra_update_mselect_rate(freqs.new);
 		if (ret) {
-			pr_err("cpu-tegra: Failed to scale mselect for cpu"
+			pr_err("tegra_update_cpu_speed: Failed to scale mselect for cpu"
 			       " frequency %u kHz\n", freqs.new);
 			return ret;
 		}
 		ret = clk_set_rate(emc_clk, tegra_emc_to_cpu_ratio(freqs.new));
 		if (ret) {
-			pr_err("cpu-tegra: Failed to scale emc for cpu"
+			pr_err("tegra_update_cpu_speed: Failed to scale emc for cpu"
 			       " frequency %u kHz\n", freqs.new);
 			return ret;
 		}
@@ -685,10 +688,15 @@ int tegra_update_cpu_speed(unsigned long rate)
 
 	ret = clk_set_rate(cpu_clk, freqs.new * 1000);
 	if (ret) {
-		pr_err("cpu-tegra: Failed to set cpu frequency to %d kHz\n",
+		pr_err("tegra_update_cpu_speed: Failed to set cpu frequency to %d kHz\n",
 			freqs.new);
 		return ret;
+	} 
+#if 0
+	else {
+		pr_info("tegra_update_cpu_speed: old=%d new=%d\n", freqs.old, tegra_getspeed(0));
 	}
+#endif
 
 	for_each_online_cpu(freqs.cpu)
 		cpufreq_notify_transition(&freqs, CPUFREQ_POSTCHANGE);
@@ -2059,8 +2067,7 @@ int tegra_cpu_set_speed_cap(unsigned int *speed_cap)
 		*speed_cap = new_speed;
 
 	ret = tegra_update_cpu_speed(new_speed);
-	if (ret == 0)
-		tegra_auto_hotplug_governor(new_speed, false);
+	tegra_auto_hotplug_governor(new_speed, false);
 	return ret;
 }
 
@@ -2289,6 +2296,9 @@ static void tegra_cpufreq_early_suspend(struct early_suspend *h)
 
 static void tegra_cpufreq_late_resume(struct early_suspend *h)
 {
+#ifdef CONFIG_TEGRA_CPUQUIET
+	tegra_cpuquiet_force_gmode();
+#endif	
 	pr_info("tegra_cpufreq_late_resume: clean cpu freq cap\n");
 	pm_qos_update_request(&cap_cpu_freq_req, (s32)PM_QOS_CPU_FREQ_MAX_DEFAULT_VALUE);
 }
