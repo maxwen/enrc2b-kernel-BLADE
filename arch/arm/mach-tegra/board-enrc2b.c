17dcebfc5fa6861c18c1f1446270dcc1ad238972
maxl <maxl@4b8ad20e-30ca-41b1-81b8-3c8b9e269e15>
2013-03-24 14:45:32 +0000

    led: set default button backlight brightness to 50

--- arch/arm/mach-tegra/board-enrc2b.c
+++ arch/arm/mach-tegra/board-enrc2b.c
@@ -227,7 +227,7 @@ static struct led_i2c_config lp5521_led_config[] = {
 	{
 		.name = "button-backlight",
 		.led_cur = 2,
-		.led_lux = 55,
+		.led_lux = 50,
 	},
 };
 static struct led_i2c_platform_data led_data = {
